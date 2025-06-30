580a907392a576541dede53e3b54b7dcd14a72b7
nizone <limanovus@gmail.com>
2025-06-30 23:18:38 +0100

    v1.1

--- languages/en.php
+++ languages/en.php
@@ -11,7 +11,16 @@
 
 // Traductions en anglais pour le composant StaffTemplate
 $en = array(
-		'staff:hello:world' => 'Hello World',
-		'' => ''
+		'staff:dipverif:admin:users:pendingvalidations' => 'Pending Validations',
+		'staff:dipverif:compatibility:error' => '<b>%s</b> cannot be enabled as long as <b>StaffDiplomaVerifier</b> is activated',
+		'staff:dipverif:email:confirmation:pending' => 'You did not confirm your email address yet - check your incoming mails, please.',
+		'staff:dipverif:diploma:upload:pending' => 'You did not confirm your email address yet - check your incoming mails, please.',
+		'staff:dipverif:activation:pending' => 'Stay tuned - the admin has been notified - your account will be activated soon.',
+		'staff:dipverif:diploma:upload:title' => 'Upload Diploma or Certification Document',
+		'staff:dipverif:admin:user:delete:error' => 'Cannot delete user! Please try again later.',
+		'staff:dipverif:admin:user:deleted' => 'User has been deleted successfully.',
+		'staff:dipverif:diploma:delete:fail' => 'Cannot delete validation document! Please try again later.',
+		'staff:dipverif:diploma:deleted' => "Validation Doc. with the title of '%s' has been successfully deleted.",
+
 );
 ossn_register_languages('en', $en);
--- ossn_com.php
+++ ossn_com.php
@@ -11,3 +11,170 @@
 
 // Informations du composant (nom, version, description, auteur, etc.)
 
+define('__STAFF_DIP_VERIF_', ossn_route()->com . 'StaffDiplomaVerifier/');
+
+require_once __STAFF_DIP_VERIF_ . 'classes/Diploma.php';
+
+function com_staff_diploma_verifier_init()
+{
+    // Hook to the default validation URL and custumize it
+	ossn_add_hook('user', 'validation:email:url', 'transformValidationUrl');
+
+    // Register the component page URL handler
+	ossn_register_page('diploma', 'diploma_upload_page_handler');
+
+    // Register the action for uploading diplomas
+	ossn_register_action('diploma/add', __STAFF_DIP_VERIF_ . 'actions/upload.php');
+
+	if (method_exists(new OssnSite, 'setSetting')) {
+		ossn_register_callback('component', 'enabled', 'com_staff_diploma_verifier_compatibility_check');
+		ossn_unregister_action('user/login');
+		ossn_register_action('user/login', __STAFF_DIP_VERIF_ . 'actions/user/login.php');
+	} else {
+		error_log('StaffDiplomaVerifier: Error version mismatch');
+		ossn_trigger_message(ossn_print('ossn:admin:settings:save:error'), 'error');
+		$comp = new OssnComponents;
+		$comp->DISABLE('StaffDiplomaVerifier');
+		redirect(REF);
+	}
+
+	if (ossn_isAdminLoggedin()) {
+			$pending_validations = array(
+				'name'   => 'staff:dipverif:admin:users:pendingvalidations',
+				'text'   => ossn_print('staff:dipverif:admin:users:pendingvalidations'),
+				'href'   => ossn_site_url('pending_validations/list'),
+				'parent' => 'admin:sidemenu:usermanager',
+			);
+			ossn_extend_view('css/ossn.admin.default', 'css/diploma_css');
+			ossn_register_page('pending_validations', 'pending_validations_page_handler');
+			ossn_register_action('ossnvds/delete', __STAFF_DIP_VERIF_ . 'actions/delete.php');
+			ossn_register_menu_item('admin/sidemenu', $pending_validations);
+	}
+	
+	ossn_extend_view('js/opensource.socialnetwork', 'js/diploma_script');
+}
+
+function com_staff_diploma_verifier_compatibility_check($event, $type, $params)
+{
+	$incompatible_coms = array('AnonymousRegistration', 'userautovalidate', 'DisableUserActivationByMail', 'PrivateNetwork');
+	if (in_array($params['component'], $incompatible_coms)) {
+		ossn_trigger_message(ossn_print('staff:dipverif:compatibility:error', array($params['component'])), 'error');
+		$com = new OssnComponents;
+		$com->DISABLE($params['component']);
+	}
+}
+
+function pending_validations_page_handler($pages){
+	$page = $pages[0];
+	if (empty($page)) {
+		echo ossn_error_page();
+	}
+	switch ($page) {
+		case 'list':
+			$title                = ossn_print('staff:dipverif:admin:users:pendingvalidations');
+			$contents['contents'] = ossn_plugin_view('pending');
+			$contents['title']    = $title;
+			$content              = ossn_set_page_layout('administrator/administrator', $contents);
+			echo ossn_view_page($title, $content, 'administrator');
+			break;	
+		
+		case 'user':
+					$diploma = new Diploma;
+					$diploma->owner_guid = $pages[1];
+					$diploma->type = 'user';
+					$diploma->subtype = 'diploma:file';
+					$success = $diploma->getDiplomaFile();
+					
+					$firstItem = $success->{'0'};
+					if (is_object($firstItem)) {
+						$filename = $firstItem->value;
+						error_log("Filename via property '0': " . $filename);
+						$firstItem->output();
+						echo $filename;
+					}
+					break;
+
+		case 'delete':
+			if(!empty($pages[1]) && !empty($pages[2])) {
+
+				//###############################################
+				// 1 Delete user from DB
+				//###############################################
+				$guid = $pages[1];	
+				if(!empty($guid)) {
+								$user = ossn_user_by_guid($guid);
+								if($user && $user->guid !== ossn_loggedin_user()->guid) {
+										if(!$user->deleteUser()) {
+												ossn_trigger_message(ossn_print('staff:dipverif:admin:user:delete:error'), 'error');
+										}
+								}	
+				}
+				ossn_trigger_message(ossn_print('admin:user:deleted'), 'success');
+			
+				//###############################################
+				// 2 Delete user's objects/entities from DB
+				//###############################################
+
+			
+				
+				}
+				redirect(REF);
+			break;
+	}
+}
+
+function transformValidationUrl($hook, $type, $return, $params) {
+	// Replace 'uservalidate/activate' with 'diploma/upload'
+    $url = str_replace('uservalidate/activate', 'diploma/upload', $return);
+    
+    // Find the last slash and extract the hash part
+    $lastSlashPos = strrpos($url, '/');
+    if ($lastSlashPos !== false) {
+        $beforeHash = substr($url, 0, $lastSlashPos + 1);
+        $hash = substr($url, $lastSlashPos + 1);
+        
+        // Keep only first 10 characters of the hash
+        $shortHash = substr($hash, 0, 10);
+        
+        // Reconstruct the URL
+        $url = $beforeHash . $shortHash;
+    }
+    
+    return $url;
+}
+
+function diploma_upload_page_handler($pages) {
+    //http://localhost/staff/diploma/upload/2/3fa88f1860 
+    if (!isset($pages[1]) || !isset($pages[2])) {
+        ossn_error_page();
+    }
+    
+    $user_guid = (int) $pages[1];
+    $token = trim($pages[2]);
+    
+    // Validate user exists
+    $user = ossn_user_by_guid($user_guid);
+    if (!$user) {
+        ossn_error_page();
+    }
+    
+    // Verify token matches first 10 chars of activation hash
+    $activation_hash = $user->activation;
+    if (empty($activation_hash) || substr($activation_hash, 0, 10) !== $token) {
+        ossn_error_page();
+    }
+    
+	// Prepare page metadata
+	ossn_set_page_owner_guid($user->guid);
+	$title = ossn_print('staff:dipverif:diploma:upload:title');
+	$content = diploma_handle_upload_logic($user);
+	$contents = array(
+		'content' => $content,
+	);
+	
+	$content = ossn_set_page_layout('contents', $contents);
+	echo ossn_view_page($title, $content);
+}
+
+
+ossn_register_callback('ossn', 'init', 'com_staff_diploma_verifier_init');
